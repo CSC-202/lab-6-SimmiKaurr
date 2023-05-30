d3bf915a4f852a8b8b41acdf3e08b2a49351f009
Simmi Kaur <122423044+SimmiKaurr@users.noreply.github.com>
2023-05-29 21:27:32 -0700

    Lab 6

--- a_recursive_list.py
+++ a_recursive_list.py
@@ -33,32 +33,83 @@ class List:
 
 
 def initialize() -> List:
+    return List()
     raise NotImplementedError("List.initialize() not defined")
 
 
 def isEmpty(data: List) -> bool:
+    if not data:
+        return True
+    elif isinstance(data, list):
+        return all(isEmpty(element) for element in data)
+    else:
+        return False
+
     raise NotImplementedError("List.isEmpty() not defined")
 
 
 def addAtIndex(data: List, index: int, value: int) -> List:
+    if index < 0:
+        raise ValueError("Index msut be non-negative.")
+    elif index == 0:
+        return [value] + data
+    elif index >= len(data):
+        return data + [value]
+    else:
+        return [data[0]] + addAtIndex(data[1:], index - 1, value)
     raise NotImplementedError("List.addAtIndex() not defined")
 
 
-def removeAtIndex(data: List, index: int) -> tuple[Node, List]:
+
+
+def removeAtIndex(head: Node, index: int) -> tuple[Node, List]:
+    if index < 0:
+        raise ValueError("Index must be non-negative.")
+    elif index == 0:
+        return head.next, [head.data] if head is not None else []
+    elif head is None:
+        raise ValueError("Index out of bounds.")
+    else:
+        new_head, removed_data = removeAtIndex(head.next, index - 1)
+        head.next = new_head
+        return head, removed_data
+    
     raise NotImplementedError("List.removeAtIndex() not defined")
 
 
 def addToFront(data: List, value: int) -> List:
+    if not data:
+        return [value]
+    else:
+        return [value] + addToFront(data, value)
     raise NotImplementedError("List.addToFront() not defined")
 
 
 def addToBack(data: List, value: int) -> List:
+    if not data:
+        return [value]
+    else:
+        return [data[0]] + addToBack(data[1:], value)
     raise NotImplementedError("List.addToBack() not defined")
 
 
 def getElementAtIndex(data: List, index: int) -> Node:
+    if index < 0:
+        raise ValueError("Index must be non-negative.")
+    elif index == 0:
+        if list.head is None:
+            raise ValueError("Index out of bounds.")
+        return list.head
+    elif list.head is None:
+        raise ValueError("Index out of bounds.")
+    else:
+        return getElementAtIndex(list.head.next, index - 1)
     raise NotImplementedError("List.getElementAtIndex() not defined")
 
 
 def clear(data: List) -> List:
+    if not data:
+        return []
+    else:
+        return clear(data[1:])
     raise NotImplementedError("List.clear() not defined")
--- b_recursive_stack.py
+++ b_recursive_stack.py
@@ -33,24 +33,55 @@ class Stack:
 
 
 def initialize() -> Stack:
+    stack = Stack()
+    return stack
     raise NotImplementedError("Stack.initialize() not defined")
 
 
 def isEmpty(data: Stack) -> bool:
+    if not data:
+        return True
+    else:
+        return False
     raise NotImplementedError("Stack.isEmpty() not defined")
 
 
 def push(data: Stack, value: int) -> Stack:
+    if not data:
+        return True
+    elif len(data) == 1 and isinstance(data[0], Stack):
+        return data[0].isEmpty()
+    else:
+        return False
     raise NotImplementedError("Stack.push() not defined")
 
 
 def pop(data: Stack) -> tuple[Node, Stack]:
+    if data.isEmpty():
+        raise ValueError("Stack is empty.")
+        
+    popped_node = data.top
+    data.top = data.top.next
+    popped_node.next = None
+
+    return popped_node, data
     raise NotImplementedError("Stack.pop() not defined")
 
 
 def peek(data: Stack) -> Node:
+    if data.isEmpty():
+        raise ValueError("Stack is empty.")
+        
+    return data.top
     raise NotImplementedError("Stack.peek() not defined")
 
 
 def clear(data: Stack) -> Stack:
+    if data.isEmpty():
+            return data
+        
+    data.top = None
+
+    return data.clear()
+
     raise NotImplementedError("Stack.clear() not defined")
